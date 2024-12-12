d9c966dcef95751de6121ba0e007eae35b3f5a3b
Clark Pagels <Cjpagels2@gmail.com>
2024-12-11 20:05:04 -0500

    cleaning
    cleaning

--- pages/grapher.py
+++ pages/grapher.py
@@ -2,16 +2,13 @@ import tkinter
 import tkinter as tk
 from tkinter import ttk
 from . import tokenizer
-from converter import converter
-import matplotlib as mpl 
-import seaborn as sb
-import tensorflow as tf
 from ..plots.zipflaw import zipf
 from ..plots.top_10_words import barChart
-from ..pages.startpage import startPage
-from ..pages.convertFile import convertFile
-from ..pages.page2 import page2 
-from ..pages.youtube_download import youtube_download 
+from startpage import startPage
+from convertFile import convertFile
+from page2 import page2 
+from .youtube_download import youtube_download 
+from ..main import frm
 
 class grapher(tk.frame): 
     def __init__(self,parent,controller):
